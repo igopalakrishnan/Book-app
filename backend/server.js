de7918c248a7af37d44fc31916c50dc7ab86b9ff
igopalakrishnan <kingkrishna809@gmail.com>
2021-11-12 11:11:07 +0530

    create route

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+//connect DB
+mongoose.connect(dbUrl, {})
+
+//Routes
+app.use('/api/users/register', (req, res) => {
+    res.send('register Route')
+})
 
 const PORT = process.env.PORT || 5000;
 
